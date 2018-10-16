2b76de3bf52c2004165d56cc19eba57e94dd849a
Mikhail <popovms1207@gmail.com>
2018-10-16 19:28:22 +0300

    //

---
+++ TwoDIndexerDemo_312/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//двумерный отказоустойчивый массив
+namespace TwoDIndexerDemo_312
+{
+    class FailSofArray2D
+    {
+        int[,] a; // ссылка на базовый двумерный массив
+        int rows, cols; //размеры массива
+        public int Length; //открытая переменная длины массива
+        public bool ErrFlag;//обозначает результат последней операции
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
